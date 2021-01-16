c3fb4f81c192939ed8491d543087d1949ef10a11
Bopsi <bshangout@gmail.com>
2021-01-16 12:54:15 +0530

    convert response to json, traverse json

--- script.py
+++ script.py
@@ -1,6 +1,12 @@
 import requests
+import json
 
+print('How many humans are in space right now?')
 resp = requests.get('http://api.open-notify.org/astros.json')
 
-print(resp.json())
+json_resp = json.loads(resp.text)
 
+print('Currently there are ' + str(json_resp['number']) + ' in space.')
+print('They are :-')
+for person in json_resp['people']:
+    print(person['name'], ' in ', person['craft'])
